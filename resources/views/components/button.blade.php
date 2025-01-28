049a6cd81cead7c05ba59d81bec09d743ec9af8f
JaumeJD <jgarveg163@iesmartinezm.es>
2025-01-28 14:23:38 +0100

    Por si lo rompo

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,15 +1,14 @@
 
-@if ($attributes->has('type')) 
-
-    <input type="{{$attributes['type']}}" name="{{$attributes['name']}}" 
-    value="{{$attributes['value']}}" class="{{$attributes['class']}}"/>
-
-@endif
-
-@if ($attributes->has('href'))
-
-    <a href="{{$attributes['href']}}" class="{{$attributes['class']}}">
-        Ver registro
+@if($attributes->has('href'))
+    <a {{$attributes->has('class') ? $attributes : $attributes->merge(['class' => 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded inline-block'])}}>
+        {{ $slot }}
     </a>
-
+@elseif($attributes->has('name'))
+    <button {{$attributes-has('class') ? $attributes : $attributes->merge(['class' => 'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'])}}>
+        {{ $slot }}
+    </button>
+@else
+    <button {{$attributes->has('class') ? $attributes : $attributes->merge(['class' => 'bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'])}}>
+        {{ $slot }}
+    </button>
 @endif
